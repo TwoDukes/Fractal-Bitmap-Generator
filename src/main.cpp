a6672665929faf1180003cd7d45d2993ea3fd713
Dustin Podell <magicdustin14@gmail.com>
2019-10-14 16:45:18 -0700

    Fractal color pops out a bit more,  temp fix for scaling

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,19 @@ int main() {
 
   for(int y = 0; y < HEIGHT; y++){
     for(int x = 0; x < WIDTH; x++){
-      double xFractal = (x - WIDTH/2) * 2.0/WIDTH;
+      double xFractal = (x - WIDTH/2 - 200) * 2.0/HEIGHT;
       double yFractal = (y - HEIGHT/2) * 2.0/HEIGHT;
 
       int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
-      uint8_t red = (uint8_t)(256 * (double)(iterations)/Mandelbrot::MAX_ITERATIONS);
+      uint8_t color = (uint8_t)(256 * (double)(iterations)/Mandelbrot::MAX_ITERATIONS);
 
-      bitmap.setPixel(x, y, red, red, red);
+      color = color*color*color; // cant overflow color due to 8-bit limit on uint_t
 
-      if(red < min) min = red;
-      if(red > max) max = red;
+      bitmap.setPixel(x, y, 0, color, 0);
+
+      if(color < min) min = color;
+      if(color > max) max = color;
     }
   }
 
